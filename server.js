29a92420062475810547d8f071e1e509ff9f8461
shivamvora <shivampatel27102001@gmail.com>
2021-06-08 23:08:25 +0530

    design done

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require( 'express' );
+const dotenv = require( 'dotenv' );
+const morgan = require( 'morgan' );
+const bodyparser = require( 'body-parser' );
+const path = require( 'path' );
+
+const app = express();
+
+dotenv.config( { path: 'config.env' } )
+
+const PORT = process.env.PORT || 8080;
+
+app.use( morgan( 'tiny' ) );
+
+// parse request to body parser
+app.use( bodyparser.urlencoded( { extended: true } ) )
+
+// set view engine
+app.set( "view engine", "ejs" );
+// app.set( "views", path.resolve( __dirname, "views/ejs" ) )
+
+// load assets
+
+app.use( '/css', express.static( path.resolve( __dirname, "assets/css" ) ) )
+app.use( '/img', express.static( path.resolve( __dirname, "assets/img" ) ) )
+app.use( '/js', express.static( path.resolve( __dirname, "assets/js" ) ) )
+
+app.get( '/', ( req, res ) => {
+    res.render( 'index' );
+} )
+
+app.listen( PORT, () => {
+    console.log( `Server is runnig on http://localhost:${PORT}` );
+} )
