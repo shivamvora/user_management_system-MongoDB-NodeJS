0def0cbcf148477d8b8aaf6317d98a2750f99222
shivamvora <shivampatel27102001@gmail.com>
2021-06-12 21:31:05 +0530

    delete user"

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -60,5 +60,17 @@ exports.update = ( req, res ) => {
 
 // Delete a user with specified
 exports.delete = ( req, res ) => {
-
+    const id = req.params.id;
+    Userdb.findByIdAndDelete( id )
+        .then( data => {
+            if ( !data ) {
+                res.status( 404 ).send( { message: `Cannot Delete with id ${id}. Maybe id is wrong` } )
+            }
+            else {
+                res.send( { message: "User was deleted successfully" } )
+            }
+        } )
+        .catch( err => {
+            res.status( 500 ).send( { message: "Could not delete User with id=" + id } );
+        } );
 }
