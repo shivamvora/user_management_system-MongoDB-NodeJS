3d808cab6c0eb1791b8861ed7f8adda3f214bc65
shivamvora <shivampatel27102001@gmail.com>
2021-06-12 20:59:22 +0530

    put request

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -40,7 +40,22 @@ exports.find = ( req, res ) => {
 
 //Update new user by user id
 exports.update = ( req, res ) => {
-
+    if ( !req.body ) {
+        return res.status( 400 )
+            .send( { message: "Data to update can not be empty" } )
+    }
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate( id, req.body, { useFindAndModify: false } )
+        .then( data => {
+            if ( !data ) {
+                res.status( 404 ).send( { message: `Can not update user with ${id} . Maybe user not found` } )
+            } else {
+                res.send( data );
+            }
+        } )
+        .catch( err => {
+            res.status( 500 ).send( { message: "Error Update user information" } )
+        } )
 }
 
 // Delete a user with specified
